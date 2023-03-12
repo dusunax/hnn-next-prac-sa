8d6f6496937a88abf443a943b2b68494c6028fbc
Dusuna <dusunax@gmail.com>
2023-03-12 23:18:37 +0900

    Fix: use-draft.ts 사용하지 않는 파일이 아니었음

---
+++ src/hooks/use-draft.ts
@@ -0,0 +1,38 @@
+// --------------------
+// 게시글 임시저장
+// --------------------
+
+import Cookies from "js-cookie";
+
+import { writeRequestType } from "@/services/post";
+
+interface UseDraftReturnType {
+  // 임시저장
+  getDraftData: () => writeRequestType | null;
+  setDraftData: (data: writeRequestType) => void;
+  clearDraftData: () => void;
+}
+
+export default function useDraft() {
+  // 임시 저장된 게시글 데이터 가져오기
+  const getDraftData: UseDraftReturnType["getDraftData"] = () => {
+    const tempData = Cookies.get("draft");
+    return tempData ? JSON.parse(tempData) : null;
+  };
+
+  // 임시 저장된 게시글 데이터 저장하기
+  const setDraftData: UseDraftReturnType["setDraftData"] = (data) => {
+    Cookies.set("draft", JSON.stringify(data));
+  };
+
+  // 임시 저장된 게시글 데이터 삭제하기
+  const clearDraftData: UseDraftReturnType["clearDraftData"] = () => {
+    Cookies.remove("draft");
+  };
+
+  return {
+    getDraftData,
+    setDraftData,
+    clearDraftData,
+  };
+}
