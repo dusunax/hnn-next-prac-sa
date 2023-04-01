77f56713db91a58f25661f345fb71e5a9b012d51
Dusuna <dusunax@gmail.com>
2023-04-01 22:50:34 +0900

    Fix: build 에러로 유저 상세 삭제

--- src/pages/[nickname]/index.tsx
+++ src/pages/[nickname]/index.tsx
@@ -1,16 +1,12 @@
 import { useRouter } from "next/router";
 
-import { UserData } from "@/models/user";
-
 import CardLayout from "@/layouts/card-layout";
 
 /** pathname의 '@nickname' 값으로 유저를 찾습니다.  */
-export default function ProfilePage({ user }: { user: UserData }) {
+export default function ProfilePage() {
   const router = useRouter();
   const { nickname } = router.query;
   if (nickname?.indexOf("@") !== 0) return <>유저를 찾을 수 없습니다.</>;
-
-  if (!user) return <>props 잘못됨</>;
 
   return <CardLayout>유저명 : {nickname}</CardLayout>;
 }
