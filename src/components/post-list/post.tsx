7ba6ed83cf8c43c62576d4a47cffb6c6472ef978
Dusuna <dusunax@gmail.com>
2023-04-02 21:24:58 +0900

    Bugfix: 비로그인 시 좋아요 버튼x, 상세 페이지 주석 처리

--- src/components/post-list/post.tsx
+++ src/components/post-list/post.tsx
@@ -7,6 +7,8 @@ import { FiHeart } from "react-icons/fi";
 import { likePostService } from "@/services/post";
 import { useState } from "react";
 import Link from "next/link";
+import { useRecoilValue } from "recoil";
+import { userState } from "@/store/user";
 
 export default function Post({ post }: { post: PostType }) {
   const {
@@ -22,6 +24,8 @@ export default function Post({ post }: { post: PostType }) {
   } = post;
   const [liked, setLiked] = useState(isPostLike === "1");
   const [viewlikeCount, setViewLikeCount] = useState(+countLike);
+
+  const user = useRecoilValue(userState);
 
   const albumCoverChange = (
     e: MouseEvent<HTMLLIElement, globalThis.MouseEvent>
@@ -87,7 +91,17 @@ export default function Post({ post }: { post: PostType }) {
             setViewLikeCount((prev) => (liked ? prev - 1 : prev + 1));
           }}
         >
-          {liked ? <FaHeart color="#ff5555" /> : <FiHeart color="#ff5555" />}
+          {!user.isLogin || liked ? (
+            <FaHeart
+              color="#ff5555"
+              className={user.isLogin ? "cursor-pointer" : ""}
+            />
+          ) : (
+            <FiHeart
+              color="#ff5555"
+              className={user.isLogin ? "cursor-pointer" : ""}
+            />
+          )}
           {viewlikeCount}
         </div>
       </div>
